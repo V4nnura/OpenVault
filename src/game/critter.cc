6126bb511d2166e6db1765db7219dfb9b1278c1b
V4nnura <52894271+V4nnura@users.noreply.github.com>
2026-09-19 09:20:09 -0300

    Update critter.cc

--- src/game/critter.cc
+++ src/game/critter.cc
@@ -725,14 +725,14 @@ void critter_kill(Object* critter, int anim, bool refresh_window)
             if (current == ANIM_FALL_BACK) {
                 back = true;
             } else {
-                fid = art_id(OBJ_TYPE_CRITTER, critter->fid & 0xFFF, ANIM_FALL_FRONT_SF, (critter->fid & 0xF000) >> 12, critter->rotation + 1);
+                fid = art_id(OBJ_TYPE_CRITTER, critter->fid & 0xFFF, ANIM_FALL_FRONT_SF, FID_WEAPON_CODE(critter->fid), critter->rotation + 1);
                 if (!art_exists(fid)) {
                     back = true;
                 }
             }
 
             if (back) {
-                fid = art_id(OBJ_TYPE_CRITTER, critter->fid & 0xFFF, ANIM_FALL_BACK_SF, (critter->fid & 0xF000) >> 12, critter->rotation + 1);
+                fid = art_id(OBJ_TYPE_CRITTER, critter->fid & 0xFFF, ANIM_FALL_BACK_SF, FID_WEAPON_CODE(critter->fid), critter->rotation + 1);
             }
 
             shouldChangeFid = true;
@@ -747,12 +747,12 @@ void critter_kill(Object* critter, int anim, bool refresh_window)
             anim = LAST_SF_DEATH_ANIM;
         }
 
-        fid = art_id(OBJ_TYPE_CRITTER, critter->fid & 0xFFF, anim, (critter->fid & 0xF000) >> 12, critter->rotation + 1);
+        fid = art_id(OBJ_TYPE_CRITTER, critter->fid & 0xFFF, anim, FID_WEAPON_CODE(critter->fid), critter->rotation + 1);
         obj_fix_violence_settings(&fid);
         if (!art_exists(fid)) {
             debug_printf("\nError: Critter Kill: Can't match fid!");
 
-            fid = art_id(OBJ_TYPE_CRITTER, critter->fid & 0xFFF, ANIM_FALL_BACK_BLOOD_SF, (critter->fid & 0xF000) >> 12, critter->rotation + 1);
+            fid = art_id(OBJ_TYPE_CRITTER, critter->fid & 0xFFF, ANIM_FALL_BACK_BLOOD_SF, FID_WEAPON_CODE(critter->fid), critter->rotation + 1);
             obj_fix_violence_settings(&fid);
         }
 
@@ -1142,7 +1142,7 @@ int critter_wake_clear(Object* obj, void* data)
 
     obj->data.critter.combat.results &= ~(DAM_KNOCKED_OUT | DAM_KNOCKED_DOWN);
 
-    int fid = art_id(FID_TYPE(obj->fid), obj->fid & 0xFFF, ANIM_STAND, (obj->fid & 0xF000) >> 12, obj->rotation + 1);
+    int fid = art_id(FID_TYPE(obj->fid), obj->fid & 0xFFF, ANIM_STAND, FID_WEAPON_CODE(obj->fid), obj->rotation + 1);
     obj_change_fid(obj, fid, 0);
 
     return 0;
